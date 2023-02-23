4b4dcbc3b8c26d038f35294ec7ecfda579670e9a
Cristina Shim <cristinashim@yahoo.com.ar>
2023-02-23 14:42:25 -0300

    api connected

--- src/home.js
+++ src/home.js
@@ -1,20 +1,31 @@
-import { getMovies } from "./services/getData";
+import { getMovies } from "./services/getData.js";
 
 const container = document.querySelector("#movies");
 
 const spinner = document.getElementById("spinner");
 
-const moviesList = async (limit = 20) => {
+const moviesList = async (title = "mandalorian") => {
   spinner.style.display = "grid";
-  const { results } = await getMovies(limit);
+  const { d } = await getMovies(title);
   spinner.style.display = "none";
 
-  results.forEach((movie) => {
+  d.forEach((items) => {
+    console.log(items);
     const article = document.createElement("article");
-    article.setAttributte("class", "movie");
+    article.setAttribute("class", "col");
     article.innerHTML = `
-            <h2>${movie.title}</h2>
+                <div class="card h-100">
+                    <img src="${items.i.imageUrl}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h2 class="card-title">${items.l}</h2>
+                            <div>
+                                <p><span>Type:</span>${items.q}</p>
+                            </div>
+                        <a href="/#/${items.id}">See Details</a>
+                    </div>
+                </div>
         `;
+    container.appendChild(article);
   });
 };
 
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,21 +1,16 @@
-const baseURL =
-  "https://imdb8.p.rapidapi.com/title/v2/get-popular-movies-by-genre";
-
-/* const options = {
+const options = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "b3bcd3fe4fmsh8e94ec6946fc3bap137a48jsn2f47e0db3d51",
     "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
   },
-}; */
+};
 
-const getMovies = async (genre) => {
-  const res = await fetch(`${baseURL}?genre=${genre}&limit=100`); /* ,
+const getMovies = async (title) => {
+  const res = await fetch(
+    `https://imdb8.p.rapidapi.com/auto-complete?q=${title}`,
     options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err)); */
+  );
   const data = await res.json();
   return data;
 };
